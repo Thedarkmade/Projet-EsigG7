5adb62910e3c373e42ce21d00825c290e0a90aef
Thedarkmade <44026072+Thedarkmade@users.noreply.github.com>
2018-11-03 14:11:54 +0100

    Add files via upload

--- ARMADA/inc/Sider.inc.php
+++ ARMADA/inc/Sider.inc.php
@@ -1,15 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
         <div class="text-left">
     		 <div class="row">
                     <div class="col-sm-3">  
                         <ul id="leftside">
-                            <li id="select"><a href="Acceuil.php" title="">Accueil</a></li><br/>
-                            <li><a href="#" title="">Connexion</a></li> <br/>
-                            <li><a href="#" title="">Inscription</a></li> <br/>
-                            <li><a href="Recherche.php" title="">Recherche</a></li> <br/>
-                            <li><a href="#" title="">Mes Bateaux</a></li> <br/>
-                            <li><a href="#" title="">Contact</a></li> <br/>
+                            <li id="<?php $p1 ?>"><a href="/ARMADA/Acceuil.php" title="">Accueil</a></li><br/>
+                            <li id="<?php $p2?>"><a href="/ARMADA/Connexion.php" title="">Connexion</a></li> <br/>
+                            <li id="<?php $p3?>"><a href="/ARMADA/Inscription.php" title="">Inscription</a></li> <br/>
+                            <li id="<?php $p4?>"><a href="/ARMADA/Recherche.php" title="">Recherche</a></li> <br/>
+                            <li id="<?php $p5?>"><a href="/ARMADA/MesBateaux.php" title="">Mes Bateaux</a></li> <br/>
+                            <li id="<?php $p6?>"><a href="/ARMADA/Contact.php" title="">Contact</a></li> <br/>
                         </ul>
                      </div>
-</html>
